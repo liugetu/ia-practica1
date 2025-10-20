ff66a7ff1fb535a9fc0b83fe1c6147e0291db0b5
jun.zhe.xia <jun.zhe.xia@estudiantat.upc.edu>
2025-10-20 15:34:30 +0200

    Script Exp2

--- comandos.sh
+++ comandos.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-#echo "Primera ejecucion Greedy1 "
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Segunda ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Tercera ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Cuarta ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Quinta ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Sexta ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Septima ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Octava ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Novena ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Decima ejecucion Greedy1"
-java -cp .:AIMA.jar:Gasolina.jar Main 1 0 >> output.txt
-#echo "Primera ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Segunda ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Tercera ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Cuarta ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Quinta ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Sexta ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Septima ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Octava ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Novena ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
-#echo "Decima ejecucion Greedy2"
-java -cp .:AIMA.jar:Gasolina.jar Main 2 0 >> output.txt
+#echo "10 ejecuciones Random, HC" "
+java -cp .:AIMA.jar:Gasolina.jar Main 0 0 10 >> output.txt
+#echo "Segunda ejecucion Greedy1, HC"
+java -cp .:AIMA.jar:Gasolina.jar Main 1 0 10 >> output.txt
+#echo "Tercera ejecucion Greedy2, HC"
+java -cp .:AIMA.jar:Gasolina.jar Main 2 0 10 >> output.txt
+#echo "Cuarta ejecucion Vacio, HC"
+java -cp .:AIMA.jar:Gasolina.jar Main 3 0 10 >> output.txt
+#echo "10 ejecuciones Random, SA" "
+java -cp .:AIMA.jar:Gasolina.jar Main 0 1 10 >> output.txt
+#echo "Segunda ejecucion Greedy1, SA"
+java -cp .:AIMA.jar:Gasolina.jar Main 1 1 10 >> output.txt
+#echo "Tercera ejecucion Greedy2, SA"
+java -cp .:AIMA.jar:Gasolina.jar Main 2 1 10 >> output.txt
+#echo "Cuarta ejecucion Vacio, SA"
+java -cp .:AIMA.jar:Gasolina.jar Main 3 1 10 >> output.txt
